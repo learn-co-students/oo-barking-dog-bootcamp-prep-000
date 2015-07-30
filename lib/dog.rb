5f42a3c222ac558a573064d1e6a646e21f1609c0
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2015-07-30 16:41:47 -0400

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,25 @@
 class Dog
-  attr_accessor :name
-
-  def bark
-    puts "woof!"
+  def name
+    @name
   end
 
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def bark
+    "woof!"
+  end
 end
 
-# instead of using attr_accessor, you could also make two methods, name and name=
-#
 # class Dog
-#   def name
-#     @name
-#   end
-#
-#   def name=(dog_name)
-#     @name = dog_name
-#   end
-#
-#   def bark
-#     puts "woof!"
-#   end
+  
+  #or, use an attr_accessor!
+
+   # attr_accessor :name
+
+   # def bark
+   #   puts "woof!"
+   # end
+
 # end
