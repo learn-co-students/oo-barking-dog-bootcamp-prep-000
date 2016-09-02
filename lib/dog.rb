f7c4028e9a31a7dbb2bf99bfffac4d37e3223487
Frankie Ferreira <frankiemferreira@gmail.com>
2016-09-02 19:51:28 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def name(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
