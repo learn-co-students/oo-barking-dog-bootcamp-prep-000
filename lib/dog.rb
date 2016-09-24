95587c30ac376962f5fc599e47371c4b877bc727
Alexander Boring <alexboring@hotmail.com>
2016-09-24 16:28:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Dog
+
+  def  name=(name)
+    @name = name
+  end
+
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark = puts "woof!"
+  end
+end
+
+jack = Dog.new
+
+jack.name
+jack.bark
