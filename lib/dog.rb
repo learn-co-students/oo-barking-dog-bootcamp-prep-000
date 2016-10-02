ceafd649ad9a4699f5d72efe429ad6d824470a5e
Joel Cowie <joel.a.cowie@gmail.com>
2016-10-02 17:13:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+
+fido.name
+fido.bark
