f53b1d878e647b62132307ca3675ae78c95b6695
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-16 05:17:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Dog
+    def initialize(name="Nameless!")
+        @name = name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def bark
+        puts "woof!"
+    end
+end
