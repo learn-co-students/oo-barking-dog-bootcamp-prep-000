5bb56b1066bcd9a34c17c2ea632caa9c021d9c40
Lily Ouyang <lilyclannad@gmail.com>
2016-12-23 23:58:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+
+     def bark
+       puts "woof!"
+     end
+
+     def name
+       @name
+     end
+
+     def name=(new_name)
+       @name=new_name
+     end
+
+end
