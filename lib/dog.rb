c67ca39faeb099c1d202bb074a9a0c59af30d570
SImon Christ <simonchrist@gmx.de>
2017-03-23 15:33:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Dog
+  # def initialize(name, breed = "Mutt")
+  #   @name = name
+  #   @breed = breed
+  # end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
