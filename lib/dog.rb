abdf9c1c255d6295a8e57fbe47de91b890c901d0
Yakov KIFFEL <jacob.kiffel@gmail.com>
2017-04-19 17:54:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+=begin
+  def initialize(name)
+    @name = name
+  end
+=end
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
