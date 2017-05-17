e0ae0dcbd7641fdc633900a46c9ae6c66420312b
Chris Wood <woodcr5@gmail.com>
2017-05-17 02:09:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def initialize=(bark)
+    @bark = "woof!"
+  end
+
+  def bark
+    @bark = "woof!"
+    puts @bark
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
