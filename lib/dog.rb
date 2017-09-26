cf6934a88944cb19e202d2e0fd133b0172b24ac9
Ion Gorincioi <gorincioii@yahoo.com>
2017-09-26 07:34:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,27 @@
+# class Dog
+#
+#   def initialize
+#     @bark = "woof!"
+#   end
+#
+#   def bark
+#     puts "woof!"
+#   end
+#
+#   def name=(name)
+#     @name = name
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+# end
+
 class Dog
 
-  def initialize
-    @bark = "woof!"
-  end
-
   def bark
-    puts @bark
+    puts "woof!"
   end
 
   def name=(name)
