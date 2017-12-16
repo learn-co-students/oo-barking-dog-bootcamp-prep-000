6b3188b5cd617fcde96982c47a9ed170121fa8f3
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-16 05:58:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+  def initialize(name="Fido")
+    @name = name
+
+  end
+
+
+  def bark
+    puts 'woof!'
+  end
+
+  def name=(name)
+    @name= name
+  end
+
+  def name
+    @name
+  end
+end
