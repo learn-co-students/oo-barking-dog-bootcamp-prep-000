237d9875959f967d45516305a2f89f9c9e5a705f
Alex Bevan <alex@bevan.eu>
2017-12-28 14:22:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Dog
+  # Class body
+
+  # Instance Method Definition
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
