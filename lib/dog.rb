bdfa2c2308e60c66807d2ccfcf7bdcb2b8e90262
Barbara Choy <barbchoy@gmail.com>
2018-01-07 07:16:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Dog
+
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
