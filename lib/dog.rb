7ebe8e5509bcfe2a71eb1425eca230a21246a539
Beth Schofield <bethmschofield@gmail.com>
2018-02-04 07:02:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def initialize(name = "Anon")
+    @name = name
+  end
+
+  def name= (newname)
+    @name = newname
+  end
+
+  def name
+    "#{@name}"
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
