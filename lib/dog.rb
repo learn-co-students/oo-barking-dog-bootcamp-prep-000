73c431ce3c3955b8dac8fd09a18adaf1b8f567f0
Felice Forby <feliceforby@gmail.com>
2018-02-08 21:32:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def initiate(name)
+    @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
