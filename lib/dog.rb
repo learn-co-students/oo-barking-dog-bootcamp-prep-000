88bdd7bf4f8713af835b743f883b59c56c9a4cb5
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-22 13:20:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+ def name=(dog_name)
+   @dog_name = dog_name
+ end
+ def name
+   @dog_name
+ end
+ def bark
+   puts "woof!"
+ end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.bark
