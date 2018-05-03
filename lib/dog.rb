09dc2921cc30cfb667afc5736adddb3b855f20c3
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-05-03 12:15:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name = (new_name)
+    @name = new_name
+  end
+
+def name
+  @name
+end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+  fido.bark
