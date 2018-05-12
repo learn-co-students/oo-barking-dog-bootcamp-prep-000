4b5bb537f59e0775b72cd171f3aeca28d4256b24
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-12 01:27:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Dog
+=begin
+  def initialize
+    @name = name
+    @breed = breed
+  end
+=end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
