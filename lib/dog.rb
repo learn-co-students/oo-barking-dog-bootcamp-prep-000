9dcd542b457eb48766fb7fbfcea0b9fa30be7523
Jed Peek <jedpeek@yahoo.com>
2018-05-17 14:52:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog
+  def bark # bark method
+    puts "woof!"
+  end
+
+  def name=(dog_name) #sets the dog name as an instance variable
+    @name = dog_name
+  end
+
+  def name #this method gets the dog name from the instace variable
+    name = @name
+  end
+
+end
