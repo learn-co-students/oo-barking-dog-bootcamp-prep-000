a9408a7f701af74a791638865f431a2961634509
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 22:06:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog
+  
+  def name=(name)
+    @name=name
+  end
+ 
+   def name
+     @name
+   end
+ 
+  def bark
+    puts "woof!"
+  end
+ 
+ 
+end
+
+rex=Dog.new
+rex.name="rec"
+puts rex.name
