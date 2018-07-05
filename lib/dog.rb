fe9108a52e6c10c4e35fe32658c1e2b173c531ac
gmat191@gmail.com <gmat191@gmail.com>
2018-07-05 22:19:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Dog # Create the Object
+  
+  # Callback function to bring the Dog Object to life.
+  def initialize(name, bark = "Woof!") 
+    @name = name 
+    @bark = bark
+  end
+  
+end
+
+fido = Dog.new #Create the Fido the Dog
+fido.bark #Asking Fido to bark
