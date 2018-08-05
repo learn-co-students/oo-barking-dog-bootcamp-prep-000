1de81c5fd94d9a734d9f376382838453b7a0346c
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-05 20:26:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Your code goes here!
+class Dog 
+  
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
