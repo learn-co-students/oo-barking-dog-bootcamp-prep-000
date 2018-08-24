24e6d0f917912bf651b96bf6f11380edc822ef6c
Viole Grace <highdevilxx@hotmail.com>
2018-08-24 23:18:10 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+  def bark()
+    puts 'woof!'
+  end
+
+  def sit()
+    puts "The Dog is sitting"
+  end
+end
