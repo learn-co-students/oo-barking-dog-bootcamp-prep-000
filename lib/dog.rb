0597da80791cbdc9dd95150e302e2f56ef91e0b0
Matthew Dizon <matthewdizon94@gmail.com>
2018-09-02 20:39:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+    def name=(names)
+        @name = names
+    end
+    def name
+        @name
+    end
+
+    def bark
+        puts "woof!"
+    end
+
+end
+fido = Dog.new
+fido.name = "Fido"
