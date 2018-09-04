5949faf0203e4c26163280fc2a41d6b9d919cdea
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-09-04 19:37:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,13 @@
-# class Dog
-#   attr_accessor :breed, :name
-#   def name=(name)
-#     @name= name 
-#   end 
-   
-#   def name 
-#     @name
-#   end 
-# end 
 
 class Dog
-  def .name=(name)
+  def name=(name)
     @name= name
+  end 
+  
+  def name
+    @name
+  end 
+  def bark 
+    puts "woof!"
   end 
 end
