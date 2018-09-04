065725039e24f17489b08a24bba952497651e689
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-09-04 19:23:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+# class Dog
+#   attr_accessor :breed, :name
+#   def name=(name)
+#     @name= name 
+#   end 
+   
+#   def name 
+#     @name
+#   end 
+# end 
+
+class Dog
+  def .name=(name)
+    @name= name
+  end 
+end
