49bdaa2a75a728a244d1b7e551d3c0e7d2012c25
Lisa Mac <lisamac421@gmail.com>
2018-10-10 18:43:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+
+class Dog
+  def initialize(name="Fido")
+    @name = name
+  end
+  
+  def name
+    @name
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  # binding.pry
+  
+  def bark
+    puts "woof!"
+  end 
+end 
+  
