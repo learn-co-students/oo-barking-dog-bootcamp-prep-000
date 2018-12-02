4c3a005ae9f84ee2eda9413c4da39bdba222e853
Kevin Kang <kevinkangg@gmail.com>
2018-12-02 02:58:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  def instance(name)
+    @name = name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def name
+    @name
+  end
+
+
+  def bark
+    puts "woof!"
+  end
+end
