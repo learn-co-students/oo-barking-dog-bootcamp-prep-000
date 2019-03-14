a41565ebcd161ec2a5d89c7de123e6e0ffd696cf
github username <github email address>
2019-03-14 07:54:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Dog
+	def name=(name) # setting the method
+		@name = name
+	end
+
+	def name # getting the method
+		@name
+	end
+
+	def bark
+		puts "woof!"
+	end
+end
