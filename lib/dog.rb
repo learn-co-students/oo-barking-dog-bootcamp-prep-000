3fbecb88bfd3b334f1b982803695e30c27d13aa8
Michael Michael <michael.ta.chan@gmail.com>
2019-04-19 04:24:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def initialize()
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(input)
+    @name = input
+  end
+  
+  def bark
+    puts "woof!"
+  end
+
+end
