6f3c885ae4a9d6450703546ea471ee523ab2b00d
dynamic-router-3131 <jtboss47@gmail.com>
2019-07-15 09:57:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog 
+
+  def name 
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
+
+fido = Dog.new
+fido.name = "Fido"
+puts fido.name
+puts fido.bark
