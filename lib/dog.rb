2a246389c019aadc57bab27b0363beeaef40c117
erin johnson <erin.nicole.johnson@gmail.com>
2019-07-23 19:52:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog
+  # def initialize(name) #required
+  #        @name = name
+  #      end
+
+       def name=(new_name)  #setter
+         @name = new_name
+       end
+
+       def name  #getter
+         @name
+       end
+
+       def bark
+         puts "woof!"
+       end
+
+end
