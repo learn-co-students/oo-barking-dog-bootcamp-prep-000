b0b9cb093f08d2ff179f540020886afa0d2734cf
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-14 22:27:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog 
+  # def name=(dog_name)
+  #   @name = dog_name
+  # end
+  # def name
+  #   @name
+  # end
+  
+  attr_accessor :name
+  
+  def bark
+    puts "woof!"
+  end
+end
