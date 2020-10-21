b901ef01d9a9919e5e934e8db5a6631df2d6e040
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 20:02:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+# defines a class called Dog
+class Dog
+  # creates #initialize to run on each instance of Dog
+  def initialize
+    # creates #writer for name
+    def name=(name)
+      @name = name
+    end
+    # creates #reader for name
+    def name
+      name = @name
+    end
+    # creats #bark that puts "woof!" when called on instance of Dog
+    def bark
+      puts "woof!"
+    end
+  end
+end
